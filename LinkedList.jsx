4061d8c05920da059f9841e32d9c07186eed8128
Shane Lupton <slupton89@gmail.com>
2018-11-08 09:22:34 -0700

    first commit

---
+++ LinkedList.jsx
@@ -0,0 +1,28 @@
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList() {
+  constructor() {
+    this.head = new null;
+  }
+
+  insertbefore()
+
+  insertAt()
+
+  insertAfter()
+
+}
+
+
+function display(sll) {
+
+}
+
+function size(sll) {
+
+}
