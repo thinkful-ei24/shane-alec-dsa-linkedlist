40fb8ccbd72306c9ddffd0049da3c7c16b93afe6
Shane Lupton <slupton89@gmail.com>
2018-11-08 09:34:51 -0700

    added find method

--- LinkedList.jsx
+++ LinkedList.jsx
@@ -10,15 +10,46 @@ class LinkedList {
     this.head = new null;
   }
 
-  insertFirst()
+  insertFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
 
-  insert()
+  insertLast(item) {
+    //check to see if empty
+    if(this.head === null) {
+      //if it is, insert new item as only item
+      this.insertFirst(item);
+    } else {
+      let tempNode = this.head;
+      //move through list until end
+      while(tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      //set the end node ptr to the new node
+      tempNode.next = new _Node(item, null);
+    }
+  }
 
-  insertLast()
-
-  remove()
-
-  find()
+  find(item) {
+    //start at the head
+    let currNode = this.head;
+    //if the list is empty
+    if(!this.head){
+      return null;
+    }
+    //check for item
+    while(currNode.value !== item){
+      //return null if end of the list and item not on list
+      if(currNode.next === null) {
+        return null;
+      } else {
+        //keep looking
+        currNode = currNode.next;
+      }
+    }
+    //found it
+    return currNode;
+  }
 
 }
 
